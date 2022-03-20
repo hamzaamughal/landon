5b49c1e2d0618ebd8386f5e6a1582243a4b1a2cd
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-03-20 20:21:26 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>My Name is Hamza Mughal.</h1>;
+  return (
+    <h1>
+      My Name is Hamza Mughal.
+      <br />I am a Software Engineer.
+    </h1>
+  );
 }
 
 export default App;
